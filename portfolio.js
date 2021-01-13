ec6f58ac644e90d2feca763b3d2bbd93705151c2
Laura Aupert <aupert.laura@gmail.com>
2021-01-12 19:10:22 -0500

    Tried to use a swiper display but did not work. Reverted to earlier code.

---
+++ portfolio.js
@@ -0,0 +1,30 @@
+var mySwiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+      
+    //   // Optional parameters
+    // direction: 'vertical',
+    // loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  })
